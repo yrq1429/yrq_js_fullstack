70d6820f4cd57b4f086829acc955ceca1fb16b0d
yrq1429 <2421064784@qq.com>
2018-05-26 22:42:55 +0800

    增添了充值页面

---
+++ wxapp/school/pages/pay/pay.js
@@ -0,0 +1,109 @@
+// pages/pay/pay.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    btn_disabled:true,
+    dorm: {
+      name: '默认寝室',
+      tip: {
+        content: '研11034',
+        color: 'green'
+      }
+    },
+    amounts: [
+      {
+        value: 100,
+        checked: false
+      },
+      {
+        value: 200,
+        checked: false
+      },
+      {
+        value: 300,
+        checked: false
+      },
+      {
+        value: 400,
+        checked: false
+      },
+      {
+        value: 500,
+        checked: false
+      },
+    ]
+  },
+  bindAmountChange(e){
+    // console.log(e.detail.value);
+    let amounts = this.data.amounts;
+    let strVal = e.detail.value;
+    for(let amounts of amounts){
+      amounts.checked = amounts.value == strVal
+    }
+    // console.log(amounts);
+    
+    this.setData({
+      amounts,
+      btn_disabled:false
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
---
+++ wxapp/school/pages/record/record.js
@@ -0,0 +1,80 @@
+// pages/record/record.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    startDate: '2018-05-25',
+    endDate: ''
+  },
+  bindDateChange (e) {
+    const type = e.currentTarget.dataset.type;
+    if (type == 'startPicker') {
+      this.setData({
+        startDate:e.detail.value,
+        endPickerStart: e.detail.value
+      })
+    }else {
+      this.setData({
+        endDate: e.detail.value,
+
+      })
+    }
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
