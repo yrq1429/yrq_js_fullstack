56c93ba6559c6edbc347e1f9aed471f8c9e2b770
yrq1429 <2421064784@qq.com>
2018-06-06 02:21:23 +0800

    初步实现搜索功能

--- wxapp/Ctrip/API/api.js
+++ wxapp/Ctrip/API/api.js
@@ -1,1 +1,1 @@
-export const API_BASE ="https://www.easy-mock.com/mock/5b1266782c0adb523338b0f7/#!method=get"
+export const API_BASE = "https://www.easy-mock.com/mock/5b1266782c0adb523338b0f7/ctrip#!method=get"
--- wxapp/Ctrip/pages/busBuy/busBuy.js
+++ wxapp/Ctrip/pages/busBuy/busBuy.js
@@ -1,88 +1,66 @@
-import {
-  API_BASE
-} from '../../API/api.js'
+// pages/busBuy/busBuy.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    searchedList: [],
-    id: 1
+  
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    //console.log(options.from+" "+options.to);
-    var from = options.from;
-    var to = options.to;
-    var temp = [];
-    wx.request({
-      url: API_BASE,
-      success: (res) => {
-        //console.log(res.data.data.trainList);
-        for(var i=0;i<res.data.data.trainList.length;i++){
-          if (from == res.data.data.trainList[i].from && to == res.data.data.trainList[i].to){
-            temp.push(res.data.data.trainList[i]);
-          }
-        }
-
-        //console.log(this.data.searchedList);
-        this.setData({
-          searchedList:temp
-        })
-      }
-    })
+  
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+  
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+  
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-
+  
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
+  
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+  
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+  
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-
+  
   }
 })
