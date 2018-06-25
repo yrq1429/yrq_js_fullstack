15c3687d730d478d5b3ebaf2f6cd4b3115e0dcd3
yrq1429 <2421064784@qq.com>
2018-06-26 00:42:29 +0800

    vue的element-ui开始学习

---
+++ vue/element-ui/src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="app">
+    <el-button loading
+    type="success">
+      提交
+    </el-button>
+<!-- // loading="el-icon-loading" -->
+    <el-button 
+    :loading = "loading"
+    icon="el-icon-edit"
+    type="primary" @click.native="changeStatus">
+    </el-button>
+    <el-button :disabled="disabled"
+    type="info">
+      提交
+    </el-button>
+
+    <el-button :disabled="disabled"
+    type="warning">
+      提交
+    </el-button>
+
+    <el-button :disabled="disabled"
+    type="danger">
+      提交
+    </el-button>
+  </div>
+</template>
+<script>
+import ElButton from '@/components/ElButton'
+export default {
+  name: 'App',
+  data () {
+    return {
+      disabled: false,
+      loading: ""
+    }
+  },
+  components: {
+    'el-button': ElButton
+  },
+  mounted () {
+    // setTimeout(() => {
+    //   this.disabled = true;
+    // }, 2000);
+  },
+  methods: {
+    changeStatus:function(){
+      console.log("aaa");
+      this.loading = "el-icon-loading"
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue/element-ui/src/components/ElButton.vue
@@ -0,0 +1,47 @@
+<template>
+  <button
+  class="el-button"
+  :class = "[
+    type? 'el-button--' + type : '',
+    {'is-disabled': buttonDisabled,
+    'is-loading': loading}
+  ]"
+  :disabled="buttonDisabled">
+    <!-- <i class="el-icon-loading" v-if="loading"></i>
+    <i v-if="icon&&!loading" :class="icon"></i> -->
+    <i v-if="loading" :class="loading" ></i>
+    <i v-if="icon&&!loading" :class="icon"></i>
+    <slot></slot>
+  </button>
+</template>
+<script>
+// el-icon-loading
+export default {
+  name: 'ElButton',
+  data(){
+      return {
+        
+      }
+  },
+  props: {
+    disabled: Boolean,
+    type: {
+      type: String,
+      default: 'default'
+    },
+    loading: String,
+    icon: {
+      type: String,
+      default: ''
+    }
+  },
+  computed: {
+    buttonDisabled () {
+      return this.disabled;
+    }
+  }
+}  
+</script>
+<style>
+
+</style>
