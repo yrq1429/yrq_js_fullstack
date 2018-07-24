af71c9f35ea091a35d131ea54162a30a3534fbe3
yrq1429 <2421064784@qq.com>
2018-07-24 21:14:37 +0800

    手写ajax以及jsonp的了解

---
+++ js/jsonp/server/app.js
@@ -0,0 +1,17 @@
+const koa = require('koa');
+const app = new koa();
+const router = require('koa-router')();
+router.get('/api', (ctx) => {
+  const data = {
+    name: 'zk',
+    age: 18
+  }
+  const callback = ctx.query.jsonpcallback;
+  if (callback) {
+  ctx.body = callback + '(' + JSON.stringify(data) + ')';    
+  }else {
+    ctx.body = body
+  }
+})
+app.use(router.routes());
+app.listen(3000)
